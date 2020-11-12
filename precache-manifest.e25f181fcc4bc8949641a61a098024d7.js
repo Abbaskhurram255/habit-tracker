2770ce980e5d32ee10e914bf3527aa9730cdeea1
Alex <39873704+Abbaskhurram255@users.noreply.github.com>
2020-11-12 21:36:21 +0500

    Updates

---
+++ precache-manifest.e25f181fcc4bc8949641a61a098024d7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70437a46a804a169772514ccb3c71817",
+    "url": "/habit-tracker/index.html"
+  },
+  {
+    "revision": "d22bb799ac5e51074434",
+    "url": "/habit-tracker/static/css/2.ff9ea1f9.chunk.css"
+  },
+  {
+    "revision": "3e1d1227294a43925c26",
+    "url": "/habit-tracker/static/css/main.f0f7d145.chunk.css"
+  },
+  {
+    "revision": "d22bb799ac5e51074434",
+    "url": "/habit-tracker/static/js/2.697b347f.chunk.js"
+  },
+  {
+    "revision": "0c200da8c2578d7778fe06f3ab474d11",
+    "url": "/habit-tracker/static/js/2.697b347f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1d1227294a43925c26",
+    "url": "/habit-tracker/static/js/main.a20fe080.chunk.js"
+  },
+  {
+    "revision": "c8d7d605d6f6f7d214d2",
+    "url": "/habit-tracker/static/js/runtime-main.a6c76754.js"
+  },
+  {
+    "revision": "0b2906ab53f22f46c646ab5835b1bca5",
+    "url": "/habit-tracker/static/media/cliplist.0b2906ab.png"
+  },
+  {
+    "revision": "148cdd9b239f013d389801c208f82a94",
+    "url": "/habit-tracker/static/media/clipshapes.148cdd9b.png"
+  }
+]);
